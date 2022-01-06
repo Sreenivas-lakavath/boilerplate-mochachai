912882c4b8d58421f83355c8a9c05c5552c3833d
Sreenivas-lakavath <83379336+Sreenivas-lakavath@users.noreply.github.com>
2022-01-05 19:44:38 -0500

    isOK

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -16,9 +16,9 @@ suite('Unit Tests', function () {
     });
     // #3
     test('#isOk, #isNotOk', function () {
-      assert.fail(null, 'null is falsey');
-      assert.fail("I'm truthy", 'A string is truthy');
-      assert.fail(true, 'true is truthy');
+      assert.isOk(null, 'null is falsey');
+      assert.isOKl("I'm truthy", 'A string is truthy');
+      assert.isNotOk(true, 'true is truthy');
     });
     // #4
     test('#isTrue, #isNotTrue', function () {
